5b17cedf00979a88c2b9a2d47e5d2789ee430af6
Anand Kumar <anand.sharma0510@gmail.com>
2020-04-16 21:27:11 +0530

    added custom tooltip in analytics page

--- src/components/Analyticts.js
+++ src/components/Analyticts.js
@@ -1,86 +1,81 @@
 import React from "react";
 import {Line} from "react-chartjs-2";
 
-const state = {
-  labels: [
-    "my tooltip 11/19",
-    "12/19",
-    "01/20",
-    "02/20",
-    "03/20",
-    "04/20",
-    "05/20",
-    "06/20",
-    "07/20",
-    "08/20",
-    "09/20",
-    "10/20",
-    "11/20",
-  ],
-  datasets: [
-    {
-      fill: false,
-      lineTension: 0.5,
-      borderColor: "rgba(0,0,0,1)",
-      pointRadius: 0,
-      data: [
-        88,
-        150,
-        180,
-        200,
-        230,
-        210,
-        170,
-        205,
-        250,
-        210,
-        270,
-        273,
-        300,
-        350,
-        380,
-      ],
-    },
-  ],
-};
+const labelData = [
+  "11/19",
+  "12/19",
+  "01/20",
+  "02/20",
+  "03/20",
+  "04/20",
+  "05/20",
+  "06/20",
+  "07/20",
+  "08/20",
+  "09/20",
+  "10/20",
+  "11/20",
+];
+const myDatasets = [
+  88,
+  150,
+  180,
+  200,
+  230,
+  210,
+  170,
+  205,
+  250,
+  210,
+  270,
+  273,
+  300,
+  350,
+  380,
+];
 
 const Chart = () => {
+  const state = {
+    labels: labelData,
+    datasets: [
+      {
+        lineTension: 0.5,
+        borderColor: "rgba(0,0,0,1)",
+        data: myDatasets,
+      },
+    ],
+  };
   return (
     <div>
       <h6>Engagment</h6>
       <Line
         data={state}
         options={{
-                responsive: true,
-                tooltips: {
-                  mode: 'index',
-                  intersect: false,
-                  callbacks:{title:()=>{return "Tolltip this is custom "}}
-                },
-               hover: {
-                  mode: 'nearest',
-                  intersect: true
-                },
-                scales: {
-                  xAxes: [{
-                    display: true,
-                    scaleLabel: {
-                      display: true,
-                      labelString: 'Month'
-                    }
-                  }],
-                  yAxes: [{
-                    display: true,
-                    scaleLabel: {
-                      display: true,
-                    },
-                  }]
-                },
-              
-          title: {
-            display: true,
+          tooltips: {
+            intersect: true,
+            callbacks: {
+              title: (tooltipItem, data) => {
+                console.log(tooltipItem, data, "title");
+                if (tooltipItem[0].xLabel === "06/20") {
+                  return "ttile :" + tooltipItem[0].yLabel;
+                } else {
 
-            fontSize: 20,
+                  return null;
+                }
+              },
+              label: (tooltipItem, data) => {
+                console.log(tooltipItem, data, "label");
+                if (tooltipItem.xLabel === "06/20") {
+                  return "custom tooltip lable:" + tooltipItem.xLabel;
+                } else {
+                  return null;
+                }
+              },
+            },
+          },
+          hover: {
+            mode: "nearest",
+            intersect: true,
           },
           legend: {
             display: true,
